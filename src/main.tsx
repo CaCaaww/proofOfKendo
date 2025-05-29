69591b2a9abebd1fee592a2987cc69591286f7e5
CaCaaww <JamesAllenHansplant@gmail.com>
2025-05-29 15:35:22 -0400

    Spring cleaning + setting up mvn and springboot.

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import '@progress/kendo-theme-default/dist/all.css';
 import Home from './Home.tsx'
-import Grid from './grid/Grid.tsx';
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/grid/",
-    element: <Grid />,
   }
 ]);
 
